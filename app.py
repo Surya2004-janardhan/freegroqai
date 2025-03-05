43d8137aba5488b21d8fdedb270b909049b30cf9
Surya2004-janardhan <121177774+Surya2004-janardhan@users.noreply.github.com>
2025-03-05 14:23:47 +0530

    Add files via upload
    added files

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import os
+from groq import Groq
+
+os.environ['GROQ_API_KEY'] = ''
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+st.title("Ask anything")
+
+user_input = st.text_input("Enter your message:")
+
+if st.button("Submit"):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+
+    st.write("Response:")
+    st.write(chat_completion.choices[0].message.content)
